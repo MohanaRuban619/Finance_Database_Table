e78bc32f7a5d32a78658c48803a8a9eaf35183a7
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-15 11:40:39 +0530

    Delete database_site/database_app directory

--- database_site/database_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import authenticate
-user = authenticate(username='john', password='secret')
-from .models import Destination
-
-
-admin.site.register(Destination)
