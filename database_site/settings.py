f0a9252736d4d57db46e3a9dd8f7c96dd484d199
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-16 19:16:33 +0530

    Update settings.py

--- database_site/settings.py
+++ database_site/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-# import django.heroku
+import django.heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 cloudinary.config( 
