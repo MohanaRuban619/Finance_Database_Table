022ed9397371984fd06999cabaf662ea1ae50a17
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-16 18:29:49 +0530

    Update admin.py

--- database_app/admin.py
+++ database_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import authenticate
 user = authenticate(username='john', password='secret')
-from .models import Destination
+from .models import Customers
 
 
-admin.site.register(Destination)
+admin.site.register(Customers)
