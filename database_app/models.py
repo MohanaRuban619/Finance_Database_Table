bf8e40065c7741cad09d50e15da00ff2454fbd43
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-16 18:31:41 +0530

    Update models.py

--- database_app/models.py
+++ database_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-class Destination(models.Model):
+class Customers(models.Model):
     Name = models.CharField(max_length=100)
     Principal_Amount = models.CharField(max_length=11)
     Intrest_Rate = models.CharField(max_length=5)
