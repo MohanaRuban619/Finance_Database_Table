99a94509c8089366fdb40ebbe462f9ad3f8c58ba
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-16 12:35:18 +0530

    Update settings.py

--- database_site/settings.py
+++ database_site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # import django.heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
