48fd144a41bcabc9dc613a5c173a5c14e703d1e3
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-18 20:26:49 +0530

    Update views.py

--- database_app/views.py
+++ database_app/views.py
@@ -4,59 +4,14 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Customer
 def datab(request):
-    
-    datas = Customer.objects.all()
-    
-    return render(request,'database.html',{'datas':datas})
-# def datae(request):
-#     datae = Destination.objects.all()
-    
-#     return render(request,'data.html',{'datae':datae})
-
-# def data(request):
-    # conn=sql
-    # print(conn)
-    # values = []
-    
-    # values.append(request.POST["Name"])
-    # values.append(request.POST["Principal_Amount"])
-    # values.append(request.POST["Interest_Rate"])
-    # values.append(request.POST["Given_Intrest"])
-    # values.append(request.POST["Given_Principle"])
-    # values.append(request.POST["Pending_Intrest"])
-    # values.append(request.POST["Balance_Principle_Amount"])
-     
-    
-    # # values=(Id_no,Name,Principal_Amount,Interest_Rate,Given_Intrest,Given_Principle,Pending_Intrest,Balance_Principle_Amount)
-    # query="insert into finance values( %s, %s, %s, %s, %s, %s, %s)"
-    # cur=conn.cursor()
-    # a=cur.execute(query, values)
-    # conn.commit()
-    # if a==1:
-    #     print("Success")
-    # else:
-        # print("Error")
-    # return render(request,'data.html',{})
-    # lis = []
-    # lis.append(request.POST["Id_no"])
-    # lis.append(request.POST["Name"])
-    # lis.append(request.POST["Principal_Amount"])
-    # lis.append(request.POST["Interest_Rate"])
-    # lis.append(request.POST["Given_Intrest"])
-    # lis.append(request.POST["Given_Principle"])
-    # lis.append(request.POST["Pending_Intrest"])
-    # lis.append(request.POST["Balance_Principle_Amount"])
-     
-    # post = lis
-    # return render(request,"b.html",{"post":post})
-
-
-# # Create your views here.
-# Id_no=int(input("Enter the id:"))
-#     Name=str(input("Enter the name:"))
-#     Principal_Amount=str(input("Enter the principal amount :"))
-#     Interest_Rate=int(input("Enter the intrest rate"))
-#     Given_Intrest=int(input("Enter the given intrest"))
-#     Given_Principle=int(input("Enter the given principal"))
-#     Pending_Intrest=int(input("Enter the pending interest"))
-#     Balance_Principle_Amount=int(input("Enter the balance principal amount"))
+    unames = ['Gopal']
+    psws = ["gopalfinance@660"]
+    name = []
+    password = []
+    name.append(request.POST["uname"])
+    password.append(request.POST["psw"])
+    if (name == unames) and ( password == psws):
+        datas = Customer.objects.all()
+        return render(request,'database.html',{'datas':datas})
+    else:
+        return render(request,'registration/login.html',{})
