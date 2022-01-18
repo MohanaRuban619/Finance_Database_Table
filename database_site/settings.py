4c6033fd786d82d685386a940bf0db7918686ed7
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-18 20:36:28 +0530

    Update settings.py

--- database_site/settings.py
+++ database_site/settings.py
@@ -155,4 +155,4 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'login/web_sites/'
+LOGIN_REDIRECT_URL = 'login/database/'
